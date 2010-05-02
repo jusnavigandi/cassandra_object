d6b2352047445cb1551acd89dc25a434a8c59f06
Cleiton Francisco <cleitonfco@gmail.com>
2010-05-02 10:28:32 -0300

    timestamps! methods generate "created_at" and "updated_at" attributes (optimized)

--- lib/cassandra_object/timestamps.rb
+++ lib/cassandra_object/timestamps.rb
@@ -6,20 +6,11 @@ module CassandraObject
       def timestamps!
         attribute :created_at, :type => :time
         attribute :updated_at, :type => :time
-        before_create :on_create
-        before_save   :on_save
-      end
-    end
 
-    module InstanceMethods
-      def on_create
-        ts = Time.now
-        self.created_at = ts
-        self.updated_at = ts
-      end
-
-      def on_save
-        self.updated_at = Time.now
+        before_save do |record|
+          record.updated_at = Time.now
+          record.created_at = record.updated_at if record.new_record?
+        end
       end
     end
   end
